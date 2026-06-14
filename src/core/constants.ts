19fe23752aad5a6c96f89ad0f868fc7c6fc57954
denisbuchenko <denis.buchenko.dev@gmail.com>
2026-06-15 02:14:28 +0300

    Change url

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -3,7 +3,7 @@ export const CANVAS_HEIGHT = 460;
 export const INITIAL_SHAPES_COUNT = 4;
 export const SLIDE_DURATION_MS = 360;
 
-export const SPRITE_IMAGE_URL = '/img.jpeg';
+export const SPRITE_IMAGE_URL = `${import.meta.env.BASE_URL}img.jpeg`;
 
 export const UI_TEXT = {
   generate: 'Сгенерировать случайную фигуру',
